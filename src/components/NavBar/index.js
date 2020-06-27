d6c4759fbd7c55ba5fe67176615579cde25e639c
edwardwang0302 <edwardwang0302@me.com>
2020-06-27 14:32:08 +0800

    add input & search component

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import PropTypes from "prop-types";
 import StyledNavBar, { StyledMenuItem, MenuIcon, MenuItems } from "./style";
 import Badge from "components/Badge";
 import Avatar from "components/Avatar";
